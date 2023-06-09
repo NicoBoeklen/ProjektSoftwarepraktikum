942ff91fff66b68ace7910da23fe9d598463484e
NicoBoeklen <nico.boeklen@uni-hohenheim.de>
2023-06-09 10:59:49 +0200

    Negotiation Service Implementation
    saveData Method

--- src/main/java/com/example/projektsoftwarepraktikum/service/NegotiationService.java
+++ src/main/java/com/example/projektsoftwarepraktikum/service/NegotiationService.java
@@ -5,6 +5,9 @@ import com.example.projektsoftwarepraktikum.repository.NegotiationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -12,11 +15,38 @@ public class NegotiationService {
     @Autowired
     private NegotiationRepository negotiationRepository;
 
+    String line = "";
+
     public Negotiation saveNegotiation(Negotiation negotiation) {
         return negotiationRepository.save(negotiation);
     }
 
     public List<Negotiation> findAllNegotiations() {
         return negotiationRepository.findAll();
+    }
+
+    public void saveNegotiationData() {
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/main/ressources/Testdaten_TKI.csv"));
+            while ((line=br.readLine()) !=null) {
+                String[] data = line.split(";");
+                if (data[0] != "NegotiationID"){    //Start with the 2 line in CSV File
+                    if (negotiationRepository.findByNegotiationId(Integer.valueOf(data[0]))==null) {
+                        //New Negotiation if negotiationID does not exist
+                        Negotiation neg = new Negotiation();
+                        neg.setNegotiationId(Integer.valueOf(data[0]));
+                        negotiationRepository.save(neg);
+                    }
+                    //Save Messages
+
+
+
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
