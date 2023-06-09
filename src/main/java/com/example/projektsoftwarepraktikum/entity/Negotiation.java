8d005a4939f1889e5e340b972637c6e2ee24603e
NicoBoeklen <nico.boeklen@uni-hohenheim.de>
2023-06-09 10:25:05 +0200

    Error Fixing

--- src/main/java/com/example/projektsoftwarepraktikum/entity/Negotiation.java
+++ src/main/java/com/example/projektsoftwarepraktikum/entity/Negotiation.java
@@ -13,7 +13,7 @@ public class Negotiation {
     private Integer negotiationId;
 
     @OneToMany(mappedBy = "negotiation")
-    @JoinTable(name = "negotiationMessage", joinColumns = @JoinColumn(name = "negotiationID"), inverseJoinColumns = @JoinColumn(name = "negotiationMessageID"))
+    //@JoinTable(name = "negotiationMessage", joinColumns = @JoinColumn(name = "negotiationID"), inverseJoinColumns = @JoinColumn(name = "negotiationMessageID"))
     private Set<NegotiationMessage> messages;
 
     public Negotiation() {
