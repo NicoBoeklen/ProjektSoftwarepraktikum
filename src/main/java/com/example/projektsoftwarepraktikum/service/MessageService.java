0626421199130db3f7943f4cb7f812bad090a149
NicoBoeklen <nico.boeklen@uni-hohenheim.de>
2023-06-09 16:20:14 +0200

    Error Fixing

--- src/main/java/com/example/projektsoftwarepraktikum/service/MessageService.java
+++ src/main/java/com/example/projektsoftwarepraktikum/service/MessageService.java
@@ -43,18 +43,18 @@ public class MessageService {
         message.setSenderId(Integer.valueOf(data[1]));
         //für neue SenderID neuen User anlegen
         newUser(Integer.valueOf(data[1]));
-        /*
-        if (data[2] != "") {
-            message.setSenderBestCase(Double.valueOf(data[2]));
+
+        if (data[2] != "" && !Objects.equals(data[2], "null")) {
+            message.setSenderBestCase(Double.valueOf(data[2].replace(",",".")));
         }
-        if (data[3] != "") {
-            message.setSenderWorstCase(Double.valueOf(data[3]));
+        if (data[3] != ""&& !Objects.equals(data[3], "null")) {
+            message.setSenderWorstCase(Double.valueOf(data[3].replace(",",".")));
         }
-        if (data[4] != "") {
-            message.setReceiversBestCase(Double.valueOf(data[4]));
+        if (data[4] != ""&& !Objects.equals(data[4], "null")) {
+            message.setReceiversBestCase(Double.valueOf(data[4].replace(",",".")));
         }
-        if (data[5] != "") {
-            message.setReceiversWorstCase(Double.valueOf(data[5]));
+        if (data[5] != ""&& !Objects.equals(data[5], "null")) {
+            message.setReceiversWorstCase(Double.valueOf(data[5].replace(",",".")));
         }
         if (data[6] != "") {
             message.setSentDate(data[6]);
@@ -65,17 +65,17 @@ public class MessageService {
         if (data[8] != "") {
             message.setNegoOutcome(data[8]);
         }
-        if (data[9] != "") {
-            message.setJointUtilityBest(Double.valueOf(data[9]));
+        if (data[9] != ""&& !Objects.equals(data[9], "null")) {
+            message.setJointUtilityBest(Double.valueOf(data[9].replace(",",".")));
         }
-        if (data[10] != "") {
-            message.setJointUtilityWorst(Double.valueOf(data[10]));
+        if (data[10] != "" && !Objects.equals(data[10], "null")) {
+            message.setJointUtilityWorst(Double.valueOf(data[10].replace(",",".")));
         }
-        if (data[11] != "") {
-            message.setContractImbalanceBest(Double.valueOf(data[11]));
+        if (data[11] != "" && !Objects.equals(data[11], "null")) {
+            message.setContractImbalanceBest(Double.valueOf(data[11].replace(",",".")));
         }
-        if (data[12] != "") {
-            message.setContractImbalanceWorst(Double.valueOf(data[12]));
+        if (data[12] != "" && !Objects.equals(data[12], "null")) {
+            message.setContractImbalanceWorst(Double.valueOf(data[12].replace(",",".")));
         }
         if (data[13] != "") {
             message.setMessageCount(Integer.valueOf(data[13]));
@@ -86,8 +86,8 @@ public class MessageService {
         if (data[15] != "") {
             message.setValue5(data[15]);
         }
-        if (data[16] != "") {
-            message.setUtility_Issue5(Double.valueOf(data[16]));
+        if (data[16] != "" && !Objects.equals(data[16], "null")) {
+            message.setUtility_Issue5(Double.valueOf(data[16].replace(",",".")));
         }
         if (data[17] != "") {
             message.setIssue4(data[17]);
@@ -95,8 +95,8 @@ public class MessageService {
         if (data[18] != "") {
             message.setValue4(data[18]);
         }
-        if (data[19] != "") {
-            message.setUtility_Issue4(Double.valueOf(data[19]));
+        if (data[19] != "" && !Objects.equals(data[19], "null")) {
+            message.setUtility_Issue4(Double.valueOf(data[19].replace(",",".")));
         }
         if (data[20] != "") {
             message.setIssue2(data[20]);
@@ -104,8 +104,8 @@ public class MessageService {
         if (data[21] != "") {
             message.setValue2(data[21]);
         }
-        if (data[22] != "") {
-            message.setUtility_Issue2(Double.valueOf(data[22]));
+        if (data[22] != "" && !Objects.equals(data[22], "null")) {
+            message.setUtility_Issue2(Double.valueOf(data[22].replace(",",".")));
         }
         if (data[23] != "") {
             message.setIssue3(data[23]);
@@ -113,8 +113,8 @@ public class MessageService {
         if (data[24] != "") {
             message.setValue3(data[24]);
         }
-        if (data[25] != "") {
-            message.setUtility_Issue3(Double.valueOf(data[25]));
+        if (data[25] != "" && !Objects.equals(data[25], "null")) {
+            message.setUtility_Issue3(Double.valueOf(data[25].replace(",",".")));
         }
         if (!Objects.equals(data[7], "INIT")) {
             if (data[26] != "") {
@@ -123,8 +123,8 @@ public class MessageService {
             if (data[27] != "") {
                 message.setValue1(data[27]);
             }
-            if (data[28] != "") {
-                message.setUtility_Issue1(Double.valueOf(data[28]));
+            if (data[28] != "" && !Objects.equals(data[28], "null")) {
+                message.setUtility_Issue1(Double.valueOf(data[28].replace(",",".")));
             }
             if (data[29] != "") {
                 message.setQ1(Integer.valueOf(data[29]));
@@ -168,22 +168,22 @@ public class MessageService {
             if (data[42] != "") {
                 message.setTKI_Accommodating_Quantile_Abs(Integer.valueOf(data[42]));
             }
-            if (data[43] != "") {
-                message.setTKI_Competing_Quantile_Rel(Double.valueOf(data[43]));
+            if (data[43] != "" && !Objects.equals(data[43], "null")) {
+                message.setTKI_Competing_Quantile_Rel(Double.valueOf(data[43].replace(",",".")));
             }
-            if (data[44] != "") {
-                message.setTKI_Compromising_Quantile_Rel(Double.valueOf(data[44]));
+            if (data[44] != "" && !Objects.equals(data[44], "null")) {
+                message.setTKI_Compromising_Quantile_Rel(Double.valueOf(data[44].replace(",",".")));
             }
-            if (data[45] != "") {
-                message.setTKI_Collaborating_Quantile_Rel(Double.valueOf(data[45]));
+            if (data[45] != "" && !Objects.equals(data[45], "null")) {
+                message.setTKI_Collaborating_Quantile_Rel(Double.valueOf(data[45].replace(",",".")));
             }
-            if (data[46] != "") {
-                message.setTKI_Avoiding_Quantile_Rel(Double.valueOf(data[46]));
+            if (data[46] != "" && !Objects.equals(data[46], "null")) {
+                message.setTKI_Avoiding_Quantile_Rel(Double.valueOf(data[46].replace(",",".")));
             }
-            if (data[47] != "") {
-                message.setTKI_Accommodating_Quantile_Rel(Double.valueOf(data[47]));
+            if (data[47] != "" && !Objects.equals(data[47], "null")) {
+                message.setTKI_Accommodating_Quantile_Rel(Double.valueOf(data[47].replace(",",".")));
             }
-        }*/
+        }
         neg.addMessage(message);
         return message;
     }
