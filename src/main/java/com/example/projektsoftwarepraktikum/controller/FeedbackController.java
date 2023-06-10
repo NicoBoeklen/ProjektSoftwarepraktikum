9853fa3563181ef6fe99f73e1845924c41e225b7
st177921 <st177921@stud.uni-stuttgart.de>
2023-06-10 15:56:52 +0200

    Hinzufügen der Controller Annotation

--- src/main/java/com/example/projektsoftwarepraktikum/controller/FeedbackController.java
+++ src/main/java/com/example/projektsoftwarepraktikum/controller/FeedbackController.java
@@ -2,8 +2,9 @@ package com.example.projektsoftwarepraktikum.controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 
-
+@Controller
 public class FeedbackController {
     @GetMapping("/feedback")
     public String startFeedback() {
