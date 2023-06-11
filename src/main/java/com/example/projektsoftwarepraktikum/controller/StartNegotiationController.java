b0fe629ffb92c1afb0c524ea7dc2036c0af042f6
st176033 <st176033@stud.uni-stuttgart.de>
2023-06-11 17:05:54 +0200

    feat: Added logout Button to Negotiation.html page

--- src/main/java/com/example/projektsoftwarepraktikum/controller/StartNegotiationController.java
+++ src/main/java/com/example/projektsoftwarepraktikum/controller/StartNegotiationController.java
@@ -1,6 +1,7 @@
 package com.example.projektsoftwarepraktikum.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,5 +16,13 @@ public class StartNegotiationController {
         public String Feedback(){
             return "redirect:/feedback";
         }
+
+   @DeleteMapping ("/login?logout") //Workaround, maybe fixing this later, since you can't have two GetMappings in one Controller
+    public String logout() {
+        return "login";
     }
+
+    }
+
+
 
