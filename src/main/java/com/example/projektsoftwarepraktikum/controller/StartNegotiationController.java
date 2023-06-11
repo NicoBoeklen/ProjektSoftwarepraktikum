44d1bccdb0ae421352a76a1c88e8e938815192d8
Remi <remi.chaieb@gmail.com>
2023-06-11 12:56:09 +0200

    redirect to feedback

--- src/main/java/com/example/projektsoftwarepraktikum/controller/StartNegotiationController.java
+++ src/main/java/com/example/projektsoftwarepraktikum/controller/StartNegotiationController.java
@@ -2,6 +2,7 @@ package com.example.projektsoftwarepraktikum.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StartNegotiationController {
@@ -9,4 +10,10 @@ public class StartNegotiationController {
     public String Negotiation() {
         return "Negotiation";
     }
-}
+
+    @PostMapping("/feedback")
+        public String Feedback(){
+            return "redirect:/feedback";
+        }
+    }
+
