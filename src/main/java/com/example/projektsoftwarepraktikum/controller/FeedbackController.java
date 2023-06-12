8b66e43dfe4eea97e24f47492bee772ebb7a40bd
Remi <remi.chaieb@gmail.com>
2023-06-12 17:34:55 +0200

    Best/Worst Case

--- src/main/java/com/example/projektsoftwarepraktikum/controller/FeedbackController.java
+++ src/main/java/com/example/projektsoftwarepraktikum/controller/FeedbackController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,27 +21,26 @@ public class FeedbackController {
     UserService userService;
 
     @GetMapping("/feedback")
-    public String startFeedback(Model model) {
+    public String startFeedback( Model model) {
         Double[] bestUtility = messageService.findAllNegotiationsMessages()
                 .stream()
-                .filter(m -> m.getSenderId() == userService.getCurrentUser().getUserId()).map(n -> n.getJointUtilityBest())
+                .filter(m -> m.getSenderId() == userService.getCurrentUser().getUserId()).map(n -> n.getSenderBestCase())
                 .filter(utility_issue1 -> utility_issue1 != null).toArray(Double[]::new);
         Double[] worstUtility = messageService.findAllNegotiationsMessages()
                 .stream()
-                .filter(m -> m.getSenderId() == userService.getCurrentUser().getUserId()).map(n -> n.getJointUtilityWorst())
+                .filter(m -> m.getSenderId() == userService.getCurrentUser().getUserId()).map(n -> n.getSenderWorstCase())
                 .filter(utility_issue1 -> utility_issue1 != null).toArray(Double[]::new);
-
        /* for(Double utilityIssue:bestUtility){
             System.out.println(utilityIssue);
         }*/
         int[] countArray = new int[worstUtility.length];
 
-        for (int i = 0; i < worstUtility.length; i++) {
+        /*for (int i = 0; i < worstUtility.length; i++) {
             countArray[i] = (i+ 1);
         }
         for(Integer count:countArray){
             System.out.println(count);
-        }
+        }*/
         model.addAttribute("countArray",countArray);
         model.addAttribute("bestUtility",bestUtility);
         model.addAttribute("worstUtility",worstUtility);
