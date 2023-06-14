db59003a545e4b64d02463b8894f7bfa76c4139c
Remi <johndoe@example.com>
2023-06-15 00:58:18 +0200

    ausgewählte Verhandlungsnummer

--- src/main/java/com/example/projektsoftwarepraktikum/controller/FeedbackController.java
+++ src/main/java/com/example/projektsoftwarepraktikum/controller/FeedbackController.java
@@ -3,32 +3,42 @@ package com.example.projektsoftwarepraktikum.controller;
 import com.example.projektsoftwarepraktikum.entity.NegotiationMessage;
 import com.example.projektsoftwarepraktikum.service.MessageService;
 import com.example.projektsoftwarepraktikum.service.UserService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
 public class FeedbackController {
-   @Autowired
+    @Autowired
     MessageService messageService;
-   @Autowired
+    @Autowired
     UserService userService;
 
     @GetMapping("/feedback")
-    public String startFeedback(@RequestParam("selectedOption") String selectedOption, Model model) {
-        //soll selectedOption nicht ein Integer sein
-        System.out.println("Ausgewählte Option: " + selectedOption);
+    public String startFeedback(@RequestParam(required = false) Integer selectedOption,Model model) {
+        System.out.println(selectedOption);
+        Integer laufendeVerhandlung;
+        //bei jedem Mal das auf /feedback zugegriffen wird WErt zurück
+        //in AJAX request WErt und bei redirect null
+        if(selectedOption!=null){
+            //muss nach laufende Verhandlung gestreamt werden
+             laufendeVerhandlung=selectedOption;
+        } else {
+            laufendeVerhandlung = null;
+        }
+        System.out.println(laufendeVerhandlung);
         Double[] bestUtility = messageService.findAllNegotiationsMessages()
                 .stream()
-                .filter(m -> m.getSenderId() == userService.getCurrentUser().getUserId()).map(n -> n.getSenderBestCase())
-                .filter(utility_issue1 -> utility_issue1 != null).toArray(Double[]::new);
+                .filter(m -> m.getSenderId() == userService.getCurrentUser().getUserId())
+                .map(n -> n.getSenderBestCase())
+                .filter(utility_issue1 -> utility_issue1 != null) .toArray(Double[]::new);
         Double[] worstUtility = messageService.findAllNegotiationsMessages()
                 .stream()
                 .filter(m -> m.getSenderId() == userService.getCurrentUser().getUserId()).map(n -> n.getSenderWorstCase())
@@ -38,11 +48,20 @@ public class FeedbackController {
         }*/
         int[] countArray = new int[worstUtility.length];
         for (int i = 0; i < worstUtility.length; i++) {
-            countArray[i] = (i+ 1);
+            countArray[i] = (i + 1);
         }
-        model.addAttribute("countArray",countArray);
-        model.addAttribute("bestUtility",bestUtility);
-        model.addAttribute("worstUtility",worstUtility);
-        return "feedback"; //Gibt die Startseite für den User zurück
+        model.addAttribute("countArray", countArray);
+        model.addAttribute("bestUtility", bestUtility);
+        model.addAttribute("worstUtility", worstUtility);
+        return "feedback";
     }
+/* Eigentlich sauberer Versuch über Post
+   Jedoch nicht funktioniert
+    @PostMapping("/feedback/submit")
+    public void startFeedback1(@RequestParam("selectedOption") String selectedOption, Model model){
+        System.out.println(selectedOption);
+
+    }
+    */
+
 }
