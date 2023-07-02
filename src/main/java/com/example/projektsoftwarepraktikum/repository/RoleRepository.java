56d25900fa26497d7bdbf662e510e7064906a280
Remi <remi.chaieb@gmail.com>
2023-07-02 17:41:21 +0200

    Query rolename

--- src/main/java/com/example/projektsoftwarepraktikum/repository/RoleRepository.java
+++ src/main/java/com/example/projektsoftwarepraktikum/repository/RoleRepository.java
@@ -3,7 +3,13 @@ package com.example.projektsoftwarepraktikum.repository;
 
 import com.example.projektsoftwarepraktikum.entity.Rolle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RoleRepository extends JpaRepository<Rolle, Integer> {
     Rolle findByRolename(String rolename);
+    @Query("SELECT r.rolename FROM Rolle r WHERE r.id = :roleId ")
+    List<String> getRolename(@Param("roleId") Integer roleId );
 }
