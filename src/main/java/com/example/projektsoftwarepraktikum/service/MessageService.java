3dde4e8f82cd1402a96a78e48670a53787ff0937
Remi <remi.chaieb@gmail.com>
2023-07-02 15:07:50 +0200

    indexe verbessert

--- src/main/java/com/example/projektsoftwarepraktikum/service/MessageService.java
+++ src/main/java/com/example/projektsoftwarepraktikum/service/MessageService.java
@@ -203,6 +203,9 @@ public class MessageService {
                 message.setTKI_Compromising_Quantile_Abs(Integer.valueOf(data[40]));
             }
             if (data[41] != "") {
+                message.setTKI_Competing_Quantile_Abs(Integer.valueOf(data[40]));
+            }
+            if (data[42] != "") {
                 message.setTKI_Avoiding_Quantile_Abs(Integer.valueOf(data[41]));
             }
             if (data[43] != "") {
